25ac93e656694cc7bd157a4077ce5cbb1b26b8cc
haidang24899 <74364083+haidang99765@users.noreply.github.com>
2021-08-14 00:06:32 +0700

    hehe

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hee luu em!',
+    introDesc: `Anh có điều này muốn hỏi em, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'oke vào công chuyện thui',
+    title: 'Em yêu anh có phải không 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là em là vợ anh đó nha :> ',
+    btnYes: 'yêu ơi là yêu lun <33',
+    btnNo: 'Em mơ à, em chọn đi đâu đấy =))))',
+    question:'Nói cho anh lý do vì sao em yêu anh đii <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Tại vì anh xỉu vì độ xinh xắn và kewt của em đó hihi <33333333',
+    mess: 'Anh biết mà 🥰. Love you 300.000 😘😘',
+    messDesc: 'còn giờ thì chờ gì nữa mà không iu anh đi nào hehe <3.',
+    btnAccept: 'Hết muối vừng lun <3',
+    messLink: 'https://www.facebook.com/ccnaizmylife/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
